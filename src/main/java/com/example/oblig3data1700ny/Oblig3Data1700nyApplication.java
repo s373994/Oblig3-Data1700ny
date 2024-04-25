8ef6e4a7cbcdfb88e9ddb8609ba287778baae21b
s373994 <s373994@oslomet.no>
2024-04-25 15:22:55 +0200

    Initial commit

---
+++ src/main/java/com/example/oblig3data1700ny/Oblig3Data1700nyApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig3data1700ny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Data1700nyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3Data1700nyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig3data1700ny/Oblig3Data1700nyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3data1700ny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Data1700nyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
